28797c29b4a79e853f9f906f76a94f8914d5aaa4
NuclearRedeye <github@mneah.com>
2020-12-27 12:29:57 +0000

    feat: initial working version

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,74 @@ let canvas: HTMLCanvasElement;
 let context: CanvasRenderingContext2D;
 let terminate: boolean = false;
 let start: number;
+let player: Player;
+
+const world = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+  [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+class Player {
+  x: number;      // The X position of the Player
+  y: number;      // The Y position of the Player
+  angle: number;  // The direction, in degrees, that the Player is looking.
+
+  constructor(x: number, y: number, angle: number) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+  }
+
+  rotate(amount: number) {
+    this.angle += amount;
+    if (this.angle < 0)
+    {
+      this.angle = 359
+    }
+    else if (this.angle >= 360)
+    {
+      this.angle = 0;
+    }
+  }
+}
+
+const width = 640;                        // The width, in pixels, of the screen.
+const height = 480;                       // The height, in pixels, of the screen.
+const halfWidth = width / 2;              // Half the width of the screen, in pixels.
+const halfHeight = height / 2;            // Half the height of the screen, in pixels.
+const columns = width;                    // The number of columns in the viewport, or basically the number or Rays to cast.
+const fieldOfView = 60;                   // The field of view, in degrees, of the camera.
+const halfFieldOfView = fieldOfView / 2;  // Half the field of view.
+
+const precision = 64;                     // The dimensions of each square in the grid.
+const increment = fieldOfView / columns;  // The step to add for each ray cast.
+
+// Converts a value from degress to radians.
+function degreeToRadians(value: number) : number {
+  return value * Math.PI / 180;
+}
+
+// Util function to draw a line.
+function drawLine(x1: number, y1: number, x2: number, y2: number, colour: string) {
+  context.strokeStyle = colour;
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+}
+
+function castRay(x: number, y: number, angle: number) : number {
+  let retVal: number = 0;
+  return retVal;
+}
 
 function onTick(timestamp: number) {
   if (start === undefined) {
@@ -11,7 +79,44 @@ function onTick(timestamp: number) {
   }
   const elapsed = timestamp - start;
 
-  context.clearRect(0, 0, canvas.width, canvas.height)
+  // Clear the Canvas, although no real need.
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  let rayAngle = player.angle - halfFieldOfView;
+  for (let i = 0; i < columns; i++) {
+    
+    // The ray starts from the players current grid position.
+    let rayX = player.x;
+    let rayY = player.y;
+
+    // These are the X and Y amounts that we need to add to check for hits against walls.
+    let rayCos = Math.cos(degreeToRadians(rayAngle)) / precision;
+    let raySin = Math.sin(degreeToRadians(rayAngle)) / precision;
+
+    // We start from the assumption that we're not already in a wall!
+    let wall = 0;
+
+    // Then, whilst we haven't hit a wall 
+    while(wall == 0) {
+        rayX += rayCos;
+        rayY += raySin;
+        wall = world[Math.floor(rayY)][Math.floor(rayX)];
+    }
+
+    // We should now have the coordinates of the wall, hence now to work out the distance.
+    let distance = Math.sqrt(Math.pow(player.x - rayX, 2) + Math.pow(player.y - rayY, 2));
+
+    // Now work out how high the wall should be...
+    let wallHeight = Math.floor(halfHeight / distance);
+
+    // And finally, draw...
+    drawLine(i, 0, i, halfHeight - wallHeight, "cyan");
+    drawLine(i, halfHeight - wallHeight, i, halfHeight + wallHeight, "red");
+    drawLine(i, halfHeight + wallHeight, i, height, "green");
+
+    // Increment the angle ready to cast the next ray.
+    rayAngle += increment;
+  }
 
   if (!terminate)
   {
@@ -25,12 +130,21 @@ window.onkeydown = (event: KeyboardEvent) => {
       terminate = true;
       break;
 
+    case "KeyA":
+      player.rotate(-1);
+      break;
+
+    case "KeyD":
+      player.rotate(1);
+      break;
+
     default:
       break;
   }
 }
 
 window.onload = function(): void {
+  player = new Player(5, 5, 0);
   canvas = document.getElementById("canvas") as HTMLCanvasElement;
   context = canvas.getContext("2d") as CanvasRenderingContext2D;
   window.requestAnimationFrame(onTick);

6134caf28d823710f0b7559c7977ac88d9b62505
NuclearRedeye <github@mneah.com>
2022-03-24 22:26:06 +0000

    refactor: remove old files

--- src/ts/utils/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { createTextElement } from './utils';
-
-describe('createTextElement', () => {
-  it('creates an element', () => {
-    const message = 'test content';
-    const result = createTextElement(message);
-    expect(result.innerHTML).toBe(message);
-  });
-});
--- src/ts/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-export function createTextElement(message: string): HTMLParagraphElement {
-  const element = document.createElement('p');
-  element.innerHTML = message;
-  return element;
-}

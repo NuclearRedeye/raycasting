8dedbefaaf7ebb5329033ef79e375d356a5ff946
NuclearRedeye <github@mneah.com>
2022-03-27 23:03:22 +0100

    chore: create an activator to register timers for door actions

--- src/ts/utils/activator-utils.ts
+++ src/ts/utils/activator-utils.ts
@@ -1,5 +1,7 @@
 import { Cell } from '../interfaces/cell';
 
+import { registerTimer } from './timer-utils.js';
+
 // Toggles the specified cells state between 0 and 1.
 export function activatorToggle(cell: Cell): void {
   cell.state = cell.state === 0 ? 1 : 0;
@@ -13,4 +15,27 @@ export function activatorIncrement(cell: Cell): void {
 // Decrements the specified cells state by 1.
 export function activatorDecrement(cell: Cell): void {
   cell.state += 1;
+}
+
+// Updates a cells state over a defined period of time.
+export function activatorDoor(cell: Cell): void {
+  // FIXME:
+  // - id should be unique
+  // - door opening/closing speed should be configurable
+  // - should not use state, as prevents door texture from being animated
+  if (cell.state === 0) {
+    // Register a new timer to CLOSE the door
+    registerTimer('testDoor', (delta: number): boolean => {
+      cell.state += (100 / 800) * delta;
+      cell.state = Math.min(cell.state, 100);
+      return cell.state >= 100;
+    });
+  } else if (cell.state === 100) {
+    // Register a new timer to OPEN the door
+    registerTimer('testDoor', (delta: number): boolean => {
+      cell.state -= (100 / 800) * delta;
+      cell.state = Math.max(cell.state, 0);
+      return cell.state <= 0;
+    });
+  }
 }

edb14d3acd75ef10bec38e7bb7ca127357a3e628
NuclearRedeye <github@mneah.com>
2024-11-19 16:37:46 +0000

    refactor: cleanup canvas sizing

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,33 +1,25 @@
-import { Point } from './interfaces/point';
 import { Rectangle } from './interfaces/rectangle';
 
 // Supported resolutions for the Back Buffer, lower resolutions yield better performance.
-const resolutions: Point[] = [
-  { x: 160, y: 120 },
-  { x: 320, y: 240 },
-  { x: 480, y: 360 },
-  { x: 640, y: 480 },
-  { x: 800, y: 600 }
+const supportedResolutions: Rectangle[] = [
+  { x: 0, y: 0, width: 160, height: 120 },
+  { x: 0, y: 0, width: 320, height: 240 },
+  { x: 0, y: 0, width: 480, height: 360 },
+  { x: 0, y: 0, width: 640, height: 480 },
+  { x: 0, y: 0, width: 800, height: 600 }
 ];
 
 // Current resolution of the Back Buffer, defaults to 640x480.
 let currentResolution = 3;
-
-export const backBufferProps: Rectangle = {
-  x: 0,
-  y: 0,
-  width: 640,
-  height: 480
-};
+export let backBufferProps: Rectangle = supportedResolutions[currentResolution];
 
 // Increases the resolution of the Back Buffer by 1, until the maximum supported resolution is reached.
 export function increaseBackBufferSize(): boolean {
-  if (currentResolution + 1 >= resolutions.length) {
+  if (currentResolution + 1 >= supportedResolutions.length) {
     return false;
   }
   currentResolution += 1;
-  backBufferProps.width = resolutions[currentResolution].x;
-  backBufferProps.height = resolutions[currentResolution].y;
+  backBufferProps = supportedResolutions[currentResolution];
   return true;
 }
 
@@ -37,7 +29,6 @@ export function decreaseBackBufferSize(): boolean {
     return false;
   }
   currentResolution -= 1;
-  backBufferProps.width = resolutions[currentResolution].x;
-  backBufferProps.height = resolutions[currentResolution].y;
+  backBufferProps = supportedResolutions[currentResolution];
   return true;
 }

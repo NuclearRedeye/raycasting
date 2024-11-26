62428207e631e44107c3cfca20b733521fb880b8
NuclearRedeye <github@mneah.com>
2024-11-26 14:55:34 +0000

    chore: cleanup comments

--- src/ts/interfaces/raycaster.ts
+++ src/ts/interfaces/raycaster.ts
@@ -5,7 +5,7 @@ export interface CastResult {
   x: number; // The X coordinate of the Cell.
   y: number; // The Y coordinate of the Cell.
   cell: Cell; // The Cell that was hit.
-  face: Face; // The specific side of the cell the ray hit.
-  wall: number; // The specific coordinates on the face that the ray hit.
+  face: Face; // The side of the cell the ray hit.
+  wall: number; // The coordinate on the face that the ray hit.
   distance: number; // The distance to the wall from the point of origin.
 }
